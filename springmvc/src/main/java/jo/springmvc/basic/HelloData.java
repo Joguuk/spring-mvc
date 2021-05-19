d65ab943a4a9a5a77fc557124f5fe9f8460972d3
joguuk <jjozerg@gmail.com>
2021-05-19 18:00:39 +0900

    message body return

---
+++ springmvc/src/main/java/jo/springmvc/basic/HelloData.java
@@ -0,0 +1,9 @@
+package jo.springmvc.basic;
+
+import lombok.Data;
+
+@Data
+public class HelloData {
+    private String username;
+    private int age;
+}
---
+++ springmvc/src/main/java/jo/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,34 @@
+package jo.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello")
+                .addObject("data", "hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello!");
+
+        return "response/hello";
+    }
+
+    /*
+    view template의 경로와 요청 url이 동일한 경우, 아무것도 반환하지 않아도 뷰 템플릿을 반환한다.
+    너무 불명확해서 비추천!
+     */
+    @RequestMapping("/response/hello")
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "hello!");
+    }
+}
