63cd4bb4c5c6a878a0f68ba8a7cbcd449d504258
joguuk <jjozerg@gmail.com>
2022-06-26 13:36:14 +0900

    <Proxy> : JDK Dynamic Proxy
    - InvocationHandler를 통해 프록시 구현
    - Proxy에 필터 적용

---
+++ proxy/src/main/java/hello/proxy/config/v2_dynamicproxy/handler/LogTraceBasicHandler.java
@@ -0,0 +1,45 @@
+package hello.proxy.config.v2_dynamicproxy.handler;
+
+import hello.proxy.trace.TraceStatus;
+import hello.proxy.trace.logtrace.LogTrace;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * packageName : hello.proxy.config.v2_dynamicproxy.handler
+ * fileName : LogTraceBasicHandler
+ * author : joguk
+ * date : 2022/06/26
+ * description :
+ * ===========================================================
+ * DATE AUTHOR NOTE
+ * 2022/06/26 joguk 최초 생성
+ * -----------------------------------------------------------
+ */
+public class LogTraceBasicHandler implements InvocationHandler {
+    private final Object target;
+    private final LogTrace logTrace;
+
+    public LogTraceBasicHandler(Object target, LogTrace logTrace) {
+        this.target = target;
+        this.logTrace = logTrace;
+    }
+
+    @Override
+    public Object invoke(Object o, Method method, Object[] args) throws Throwable {
+        TraceStatus status = null;
+        try {
+            String message = method.getDeclaringClass().getSimpleName() + "." + method.getName() + "()";
+            status = logTrace.begin(message);
+
+            // 로직 호출
+            Object result = method.invoke(target, args);
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+}
